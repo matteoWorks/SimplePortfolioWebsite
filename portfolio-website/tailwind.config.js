77a97f1e24b26ca0b0aa3bc7f1cc16ebdeef6596
Matteo <109966451+ShameIRL@users.noreply.github.com>
2024-11-08 10:27:29 +0100

    Defined "public"

---
+++ portfolio-website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
